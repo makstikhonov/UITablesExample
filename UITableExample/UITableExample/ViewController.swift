c9cf31289f89f638b76d0a942ca7ca03ed2c24be
Maks T <mad-d@yandex.ru>
2022-01-19 12:23:59 +0300

    start

---
+++ UITableExample/UITableExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableExample
+//
+//  Created by max on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
